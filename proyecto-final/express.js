b2c78b6c4cb10b19fc1f11f55836dd7d9f69cde8
Paola <pmunoz@unah.hn>
2023-12-05 23:17:31 -0600

    Update express.js

--- proyecto-final/express.js
+++ proyecto-final/express.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const productos = require('./productos/productos.json')
-const { validateProduct } = require('./schemas/producto')
-const { randomUUID } = require('crypto');
+const express = require("express");
+const app = express();
 
-const app = express()
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+app.use(indexRoutes);
 
-
-const port = 3000
-
-
+module.exports = app;
