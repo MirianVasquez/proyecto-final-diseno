f6d278fd8bd7f856beaf44b9a83cae7254d5143e
MirianVasquez <MiriEster.Vasquez@gmail.com>
2023-12-09 12:37:23 -0600

    .

--- proyecto-final/ServerSide/Index.js
+++ proyecto-final/ServerSide/Index.js
@@ -21,8 +21,9 @@ function authenticateToken(req, res, next) {
   const token = authHeader && authHeader.split(" ")[1];
 
   if (token == null) return res.sendStatus(401);
+  
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, user) => {
     if (err) {
       console.error("JWT Verification Error:", err);
       return res.sendStatus(403);
